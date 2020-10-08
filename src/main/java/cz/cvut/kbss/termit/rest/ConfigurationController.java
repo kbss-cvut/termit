dc5c2545dfe9b3f9ab8814f66899ac4de863695e
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-10-08 17:30:05 +0200

    [Task #1333] Add REST API for getting basic configuration from server to  clients.
    Currently, this configuration contains only language, but extensions are expected.

---
+++ src/main/java/cz/cvut/kbss/termit/rest/ConfigurationController.java
@@ -0,0 +1,29 @@
+package cz.cvut.kbss.termit.rest;
+
+import cz.cvut.kbss.termit.dto.ConfigurationDto;
+import cz.cvut.kbss.termit.security.SecurityConstants;
+import cz.cvut.kbss.termit.service.config.ConfigurationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@PreAuthorize("hasRole('" + SecurityConstants.ROLE_USER + "')")
+@RestController
+@RequestMapping("/configuration")
+public class ConfigurationController {
+
+    private final ConfigurationProvider configProvider;
+
+    @Autowired
+    public ConfigurationController(ConfigurationProvider configProvider) {
+        this.configProvider = configProvider;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ConfigurationDto getConfiguration() {
+        return configProvider.getConfiguration();
+    }
+}
