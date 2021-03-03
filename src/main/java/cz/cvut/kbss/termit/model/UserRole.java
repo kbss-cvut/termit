bb9187374fd691c2dd8f6c5f1aa9956d7397bbc2
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-03-03 14:47:06 +0100

    [Bug #1510] Allow unauthenticated clients to get basic configuration as well.

--- src/main/java/cz/cvut/kbss/termit/model/UserRole.java
+++ src/main/java/cz/cvut/kbss/termit/model/UserRole.java
@@ -1,16 +1,13 @@
 /**
  * TermIt Copyright (C) 2019 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this program.  If
- * not, see <https://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package cz.cvut.kbss.termit.model;
@@ -21,7 +18,9 @@ import cz.cvut.kbss.jopa.model.annotations.OWLAnnotationProperty;
 import cz.cvut.kbss.jopa.model.annotations.OWLClass;
 import cz.cvut.kbss.termit.model.util.HasIdentifier;
 import cz.cvut.kbss.termit.util.Vocabulary;
+
 import java.net.URI;
+import java.util.Objects;
 
 @OWLClass(iri = Vocabulary.s_c_uzivatelska_role)
 public class UserRole implements HasIdentifier {
@@ -35,11 +34,13 @@ public class UserRole implements HasIdentifier {
     @OWLAnnotationProperty(iri = Vocabulary.s_p_scopeNote)
     private MultilingualString description;
 
-    @Override public URI getUri() {
+    @Override
+    public URI getUri() {
         return uri;
     }
 
-    @Override public void setUri(URI uri) {
+    @Override
+    public void setUri(URI uri) {
         this.uri = uri;
     }
 
@@ -57,5 +58,27 @@ public class UserRole implements HasIdentifier {
 
     public void setDescription(MultilingualString description) {
         this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserRole userRole = (UserRole) o;
+        return Objects.equals(uri, userRole.uri);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uri);
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" + label + " <" + uri + ">}";
     }
 }
