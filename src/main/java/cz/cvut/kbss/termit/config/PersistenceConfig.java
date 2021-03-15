b24cdf6fa56e1d7b353a2bd95c2cdcff355d908f
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-03-15 10:25:20 +0100

    [Enhancement #50] Ensure JavaMelody monitoring page is secured. Minor code cleanup.

--- src/main/java/cz/cvut/kbss/termit/config/PersistenceConfig.java
+++ src/main/java/cz/cvut/kbss/termit/config/PersistenceConfig.java
@@ -17,16 +17,13 @@ package cz.cvut.kbss.termit.config;
 import com.github.ledsoft.jopa.spring.transaction.DelegatingEntityManager;
 import com.github.ledsoft.jopa.spring.transaction.JopaTransactionManager;
 import cz.cvut.kbss.jopa.model.EntityManagerFactory;
-import cz.cvut.kbss.termit.persistence.MainPersistenceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@Import(MainPersistenceFactory.class)
 public class PersistenceConfig {
 
     @Bean
