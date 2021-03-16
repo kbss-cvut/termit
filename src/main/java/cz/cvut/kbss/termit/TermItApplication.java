5425560e7fc3a10e222843bcd39782ef17d16ff9
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-03-16 10:50:23 +0100

    [Enhancement #50] Allow building project both as JAR and WAR based on Maven profiles.

--- src/main/java/cz/cvut/kbss/termit/TermItApplication.java
+++ src/main/java/cz/cvut/kbss/termit/TermItApplication.java
@@ -5,13 +5,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.util.Collections;
 
 @SpringBootApplication
-public class TermItApplication {
+public class TermItApplication extends SpringBootServletInitializer {
 
     private static final Logger LOG = LoggerFactory.getLogger(TermItApplication.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TermItApplication.class);
+    }
 
     public static void main(String[] args) {
         printStartupMessage();
