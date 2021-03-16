0ec86b1529ecf50d5cd7ec0e1475632522453cbd
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-03-16 13:38:28 +0100

    [Fix] Another attempt at fixing the unstable GitHub build.

--- src/test/java/cz/cvut/kbss/termit/service/BaseServiceTestRunner.java
+++ src/test/java/cz/cvut/kbss/termit/service/BaseServiceTestRunner.java
@@ -14,17 +14,12 @@ package cz.cvut.kbss.termit.service;
 import cz.cvut.kbss.jopa.model.EntityManager;
 import cz.cvut.kbss.termit.environment.TransactionalTestRunner;
 import cz.cvut.kbss.termit.environment.config.TestConfig;
-import cz.cvut.kbss.termit.environment.config.TestPersistenceAspectsConfig;
 import cz.cvut.kbss.termit.environment.config.TestPersistenceConfig;
 import cz.cvut.kbss.termit.environment.config.TestServiceConfig;
-import cz.cvut.kbss.termit.service.jmx.AppAdminBean;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -40,6 +35,7 @@ import java.net.URI;
         TestPersistenceConfig.class,
         TestServiceConfig.class}, initializers = {ConfigDataApplicationContextInitializer.class})
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class BaseServiceTestRunner extends TransactionalTestRunner {
 
     private static final String EXISTENCE_CHECK_QUERY = "ASK { ?x a ?type . }";
