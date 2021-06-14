eac7c1559f1afc8047d63cb6d5c4f2adeadcd7e6
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-06-14 13:02:20 +0200

    [Code style] Update .editorconfig with IDEA-specific configuration to minimize formatting differences between machines.

--- src/main/java/cz/cvut/kbss/termit/persistence/dao/VocabularyDao.java
+++ src/main/java/cz/cvut/kbss/termit/persistence/dao/VocabularyDao.java
@@ -1,13 +1,16 @@
 /**
  * TermIt Copyright (C) 2019 Czech Technical University in Prague
  * <p>
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * <p>
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
  * <p>
- * You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.termit.persistence.dao;
 
@@ -154,7 +157,8 @@ public class VocabularyDao extends AssetDao<Vocabulary> implements SupportsLastM
     /**
      * Updates glossary contained in the specified vocabulary.
      * <p>
-     * The vocabulary is passed for correct context resolution, as glossary existentially depends on its owning vocabulary.
+     * The vocabulary is passed for correct context resolution, as glossary existentially depends on its owning
+     * vocabulary.
      *
      * @param entity Owner of the updated glossary
      * @return The updated entity
@@ -165,7 +169,8 @@ public class VocabularyDao extends AssetDao<Vocabulary> implements SupportsLastM
     }
 
     /**
-     * Checks whether terms from the {@code subjectVocabulary} reference (as parent terms) any terms from the {@code targetVocabulary}.
+     * Checks whether terms from the {@code subjectVocabulary} reference (as parent terms) any terms from the {@code
+     * targetVocabulary}.
      *
      * @param subjectVocabulary Subject vocabulary identifier
      * @param targetVocabulary  Target vocabulary identifier
