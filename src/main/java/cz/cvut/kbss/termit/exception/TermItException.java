d601f4350728649984c003088a148d900e41d96e
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-06-30 16:32:11 +0200

    Suppress logging of the UnsupportedOperationException thrown by TermRepositoryService.getUnusedTermsInVocabulary as it was plaguing the logs with repeated useless stack traces.

--- src/main/java/cz/cvut/kbss/termit/exception/TermItException.java
+++ src/main/java/cz/cvut/kbss/termit/exception/TermItException.java
@@ -1,19 +1,16 @@
 /**
- * TermIt
- * Copyright (C) 2019 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * TermIt Copyright (C) 2019 Czech Technical University in Prague
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * <p>
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.termit.exception;
 
@@ -24,18 +21,37 @@ package cz.cvut.kbss.termit.exception;
  */
 public class TermItException extends RuntimeException {
 
+    protected final boolean suppressLogging;
+
     protected TermItException() {
+        this.suppressLogging = false;
     }
 
     public TermItException(String message) {
+        this(message, false);
+    }
+
+    public TermItException(String message, boolean suppressLogging) {
         super(message);
+        this.suppressLogging = suppressLogging;
     }
 
     public TermItException(String message, Throwable cause) {
         super(message, cause);
+        this.suppressLogging = false;
     }
 
     public TermItException(Throwable cause) {
         super(cause);
+        this.suppressLogging = false;
+    }
+
+    /**
+     * Whether logging of this exception should be suppressed.
+     *
+     * @return True if exception should not be logged
+     */
+    public boolean shouldSuppressLogging() {
+        return suppressLogging;
     }
 }
