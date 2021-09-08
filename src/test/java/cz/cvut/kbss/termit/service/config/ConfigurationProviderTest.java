330ebe99d9521a1ffdd4dabf3bbd298c92304eb0
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-09-08 13:42:56 +0200

    [Bug #1620] Provide configured max upload file size to the client in Configuration.

--- src/test/java/cz/cvut/kbss/termit/service/config/ConfigurationProviderTest.java
+++ src/test/java/cz/cvut/kbss/termit/service/config/ConfigurationProviderTest.java
@@ -3,12 +3,12 @@ package cz.cvut.kbss.termit.service.config;
 import cz.cvut.kbss.termit.dto.ConfigurationDto;
 import cz.cvut.kbss.termit.service.BaseServiceTestRunner;
 import cz.cvut.kbss.termit.service.repository.UserRoleRepositoryService;
-import cz.cvut.kbss.termit.util.ConfigParam;
 import cz.cvut.kbss.termit.util.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,11 +23,14 @@ class ConfigurationProviderTest extends BaseServiceTestRunner {
     @Autowired
     private UserRoleRepositoryService service;
 
+    @Autowired
+    private Environment environment;
+
     private ConfigurationProvider sut;
 
     @BeforeEach
     void setUp() {
-        this.sut = new ConfigurationProvider(config, service);
+        this.sut = new ConfigurationProvider(config, service, environment);
     }
 
     @Test
@@ -35,5 +38,6 @@ class ConfigurationProviderTest extends BaseServiceTestRunner {
         final ConfigurationDto result = sut.getConfiguration();
         assertNotNull(result);
         assertEquals(config.getPersistence().getLanguage(), result.getLanguage());
+        assertEquals(environment.getProperty("spring.servlet.multipart.max-file-size"), result.getMaxFileUploadSize());
     }
 }
