af84f40cc4a11a3221f649e7135efee10baf1556
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-09-20 09:13:04 +0200

    [Bug #1620] Minor improvements of the maxUploadFileSize configuration retrieval.

--- src/test/java/cz/cvut/kbss/termit/service/config/ConfigurationProviderTest.java
+++ src/test/java/cz/cvut/kbss/termit/service/config/ConfigurationProviderTest.java
@@ -4,22 +4,19 @@ import cz.cvut.kbss.termit.dto.ConfigurationDto;
 import cz.cvut.kbss.termit.service.BaseServiceTestRunner;
 import cz.cvut.kbss.termit.util.Configuration;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.beans.factory.annotation.Value;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@ExtendWith(SpringExtension.class)
 class ConfigurationProviderTest extends BaseServiceTestRunner {
 
     @Autowired
     private Configuration config;
 
-    @Autowired
-    private Environment environment;
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maxFileSize;
 
     @Autowired
     private ConfigurationProvider sut;
@@ -29,6 +26,6 @@ class ConfigurationProviderTest extends BaseServiceTestRunner {
         final ConfigurationDto result = sut.getConfiguration();
         assertNotNull(result);
         assertEquals(config.getPersistence().getLanguage(), result.getLanguage());
-        assertEquals(environment.getProperty("spring.servlet.multipart.max-file-size"), result.getMaxFileUploadSize());
+        assertEquals(maxFileSize, result.getMaxFileUploadSize());
     }
 }
