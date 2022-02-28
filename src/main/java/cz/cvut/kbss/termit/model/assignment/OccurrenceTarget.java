a1fcf9f1f084d97dec46eac0a739179b979acda0
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2022-02-28 14:52:36 +0100

    [Task #1652] Separate TermOccurrence and OccurrenceTarget from TermAssignment and Target.

--- src/main/java/cz/cvut/kbss/termit/model/assignment/OccurrenceTarget.java
+++ src/main/java/cz/cvut/kbss/termit/model/assignment/OccurrenceTarget.java
@@ -1,14 +1,23 @@
 package cz.cvut.kbss.termit.model.assignment;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import cz.cvut.kbss.jopa.model.annotations.*;
+import cz.cvut.kbss.termit.model.AbstractEntity;
 import cz.cvut.kbss.termit.model.Asset;
 import cz.cvut.kbss.termit.model.selector.Selector;
 import cz.cvut.kbss.termit.util.Vocabulary;
 
+import java.net.URI;
+import java.util.Objects;
 import java.util.Set;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "javaClass")
 @OWLClass(iri = Vocabulary.s_c_cil_vyskytu)
-public abstract class OccurrenceTarget extends Target {
+public abstract class OccurrenceTarget extends AbstractEntity {
+
+    @ParticipationConstraints(nonEmpty = true)
+    @OWLObjectProperty(iri = Vocabulary.s_p_ma_zdroj)
+    private URI source;
 
     @ParticipationConstraints(nonEmpty = true)
     @OWLObjectProperty(iri = Vocabulary.s_p_ma_selektor, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
@@ -17,8 +26,16 @@ public abstract class OccurrenceTarget extends Target {
     public OccurrenceTarget() {
     }
 
-    public OccurrenceTarget(Asset source) {
-        super(source);
+    public OccurrenceTarget(Asset<?> source) {
+        this.source = Objects.requireNonNull(source).getUri();
+    }
+
+    public URI getSource() {
+        return source;
+    }
+
+    public void setSource(URI source) {
+        this.source = source;
     }
 
     public Set<Selector> getSelectors() {
@@ -31,8 +48,9 @@ public abstract class OccurrenceTarget extends Target {
 
     @Override
     public String toString() {
-        return "OccurrenceTarget{" +
-                "selectors=" + selectors +
+        return "OccurrenceTarget{<" + getUri() +
+                ">, source=<" + source +
+                ">, selectors=" + selectors +
                 "} " + super.toString();
     }
 }
