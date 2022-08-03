e17e89c6b8c67aa1e99c985a481a0626ad26c76d
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2022-08-03 17:32:47 +0200

    [#164] Resolve vocabulary context based on workspace (editable vocabularies).
    If a vocabulary is editable, use its context instead of the canonical one.

--- src/test/java/cz/cvut/kbss/termit/environment/config/TestServiceConfig.java
+++ src/test/java/cz/cvut/kbss/termit/environment/config/TestServiceConfig.java
@@ -1,13 +1,16 @@
 /**
  * TermIt Copyright (C) 2019 Czech Technical University in Prague
  * <p>
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * <p>
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
  * <p>
- * You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.termit.environment.config;
 
@@ -17,8 +20,6 @@ import cz.cvut.kbss.termit.environment.Environment;
 import cz.cvut.kbss.termit.model.selector.Selector;
 import cz.cvut.kbss.termit.service.document.html.DummySelectorGenerator;
 import cz.cvut.kbss.termit.service.document.html.HtmlSelectorGenerators;
-import cz.cvut.kbss.termit.util.Configuration;
-import cz.cvut.kbss.termit.workspace.EditableVocabularies;
 import org.aspectj.lang.Aspects;
 import org.jsoup.nodes.Element;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -97,10 +98,5 @@ public class TestServiceConfig {
     @Primary
     public ApplicationEventPublisher eventPublisher() {
         return mock(ApplicationEventPublisher.class);
-    }
-
-    @Bean
-    public EditableVocabularies editableVocabularies(Configuration config) {
-        return new EditableVocabularies(config);
     }
 }
