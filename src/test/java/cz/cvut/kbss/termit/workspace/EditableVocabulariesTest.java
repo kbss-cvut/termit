7fcd8aca0fa1afb0b0ea73ddf6682541973c1e69
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2022-08-08 14:06:12 +0200

    [#164] Add AuthorizationService that determines whether vocabulary is editable.

--- src/test/java/cz/cvut/kbss/termit/workspace/EditableVocabulariesTest.java
+++ src/test/java/cz/cvut/kbss/termit/workspace/EditableVocabulariesTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
-import java.util.Collections;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
