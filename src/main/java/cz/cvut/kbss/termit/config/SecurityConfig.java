483e66b1b15a37e596924349bb9f9a6c965feded
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2022-08-09 16:39:56 +0200

    [Chore #164] Remove unused import.

--- src/main/java/cz/cvut/kbss/termit/config/SecurityConfig.java
+++ src/main/java/cz/cvut/kbss/termit/config/SecurityConfig.java
@@ -29,7 +29,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
