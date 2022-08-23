5845455a01f67a1ee1f7a1c3a50fad59a497952c
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2022-08-23 10:23:47 +0200

    Log session creation events.

--- src/main/java/cz/cvut/kbss/termit/config/SessionTimeoutManager.java
+++ src/main/java/cz/cvut/kbss/termit/config/SessionTimeoutManager.java
@@ -1,6 +1,8 @@
 package cz.cvut.kbss.termit.config;
 
 import cz.cvut.kbss.termit.security.SecurityConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSessionEvent;
@@ -9,8 +11,11 @@ import javax.servlet.http.HttpSessionListener;
 @WebListener
 public class SessionTimeoutManager implements HttpSessionListener {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SessionTimeoutManager.class);
+
     @Override
     public void sessionCreated(HttpSessionEvent se) {
+        LOG.trace("Created session {}.", se.getSession().getId());
         se.getSession().setMaxInactiveInterval(SecurityConstants.SESSION_TIMEOUT);
     }
 }
