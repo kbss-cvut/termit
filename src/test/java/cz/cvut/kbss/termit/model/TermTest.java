4bc346b13be30ef7d247aaa0d4b15f4e8895314c
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2022-10-31 15:08:25 +0100

    [kbss-cvut/termit-ui#341] Extend ReadOnlyTerm model with skos:notation and skos:example.
    Fix Term tests.

--- src/test/java/cz/cvut/kbss/termit/model/TermTest.java
+++ src/test/java/cz/cvut/kbss/termit/model/TermTest.java
@@ -104,7 +104,7 @@ class TermTest {
         term.setTypes(new LinkedHashSet<>(Arrays.asList(Vocabulary.s_c_object, Vocabulary.s_c_entity)));
         final String result = term.toCsv();
         final String[] items = result.split(",");
-        assertThat(items.length, greaterThanOrEqualTo(5));
+        assertThat(items.length, greaterThanOrEqualTo(7));
         final String types = items[6];
         assertTrue(types.matches(".+;.+"));
         term.getTypes().forEach(t -> assertTrue(types.contains(t)));
@@ -117,7 +117,7 @@ class TermTest {
                 Arrays.asList(Generator.generateUri().toString(), "PSP/c-1/p-2/b-c", "PSP/c-1/p-2/b-f")));
         final String result = term.toCsv();
         final String[] items = result.split(",");
-        assertThat(items.length, greaterThanOrEqualTo(6));
+        assertThat(items.length, greaterThanOrEqualTo(8));
         final String sources = items[7];
         assertTrue(sources.matches(".+;.+"));
         term.getSources().forEach(t -> assertTrue(sources.contains(t)));
@@ -129,7 +129,7 @@ class TermTest {
         term.setParentTerms(new HashSet<>(Generator.generateTermsWithIds(5)));
         final String result = term.toCsv();
         final String[] items = result.split(",");
-        assertThat(items.length, greaterThanOrEqualTo(7));
+        assertThat(items.length, greaterThanOrEqualTo(9));
         final String parentTerms = items[8];
         assertTrue(parentTerms.matches(".+;.+"));
         term.getParentTerms().forEach(t -> assertTrue(parentTerms.contains(t.getUri().toString())));
@@ -141,8 +141,8 @@ class TermTest {
         term.setSubTerms(IntStream.range(0, 5).mapToObj(i -> generateTermInfo()).collect(Collectors.toSet()));
         final String result = term.toCsv();
         final String[] items = result.split(",");
-        assertThat(items.length, greaterThanOrEqualTo(15));
-        final String subTerms = items[14];
+        assertThat(items.length, greaterThanOrEqualTo(10));
+        final String subTerms = items[9];
         assertTrue(subTerms.matches(".+;.+"));
         term.getSubTerms().forEach(t -> assertTrue(subTerms.contains(t.getUri().toString())));
     }
