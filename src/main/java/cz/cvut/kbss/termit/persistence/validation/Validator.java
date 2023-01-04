75ff94ed095b4ecd2776a3f4aa31e9460e0727e1
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2023-01-04 17:18:07 +0100

    [kbss-cvut/termit-ui#184] Override Czech-specific validation rules to support i18n of TermIt deployments.

--- src/main/java/cz/cvut/kbss/termit/persistence/validation/Validator.java
+++ src/main/java/cz/cvut/kbss/termit/persistence/validation/Validator.java
@@ -5,10 +5,13 @@ import cz.cvut.kbss.jopa.model.EntityManager;
 import cz.cvut.kbss.jopa.model.MultilingualString;
 import cz.cvut.kbss.termit.exception.TermItException;
 import cz.cvut.kbss.termit.model.validation.ValidationResult;
+import cz.cvut.kbss.termit.util.Configuration;
+import cz.cvut.kbss.termit.util.Utils;
 import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.util.FileUtils;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
@@ -18,6 +21,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,13 +42,30 @@ public class Validator implements VocabularyContentValidator {
 
     private static final Logger LOG = LoggerFactory.getLogger(Validator.class);
 
+    /**
+     * TermIt overrides some glossary validation rules predefined by SGoV.
+     * <p>
+     * The main reason for overriding is missing internationalization of the built-in rules.
+     */
+    private static final Set<String> GLOSSARY_RULES_TO_OVERRIDE = Set.of("g2.ttl", "g4.ttl");
+
+    /**
+     * Model validation rules to add.
+     * <p>
+     * TermIt does not support all the modeling features validated by SGoV validator. So we use only those we support.
+     */
+    private static final Set<String> MODEL_RULES_TO_ADD = Set.of("m1.ttl", "m2.ttl");
+
     private final org.eclipse.rdf4j.repository.Repository repository;
     private final ValueFactory vf;
 
+    private final String language;
+
     @Autowired
-    public Validator(EntityManager em) {
+    public Validator(EntityManager em, Configuration config) {
         this.repository = em.unwrap(org.eclipse.rdf4j.repository.Repository.class);
         vf = repository.getValueFactory();
+        this.language = config.getPersistence().getLanguage();
     }
 
     private Model getModelFromRdf4jRepository(final Collection<URI> vocabularyIris)
@@ -57,8 +79,8 @@ public class Validator implements VocabularyContentValidator {
         writer.close();
         final byte[] savedData = baos.toByteArray();
         final ByteArrayInputStream bais = new ByteArrayInputStream(savedData);
-        org.apache.jena.rdf.model.Model model = ModelFactory.createDefaultModel();
-        model.read(bais, null, "TURTLE");
+        Model model = ModelFactory.createDefaultModel();
+        model.read(bais, null, FileUtils.langTurtle);
         return model;
     }
 
@@ -67,30 +89,24 @@ public class Validator implements VocabularyContentValidator {
     public List<ValidationResult> validate(final Collection<URI> vocabularyIris) {
         LOG.debug("Validating {}", vocabularyIris);
         final com.github.sgov.server.Validator validator = new com.github.sgov.server.Validator();
-        final Set<URL> rules = new HashSet<>();
-        rules.addAll(validator.getGlossaryRules());
-        rules.addAll(
-                // Currently, only using content rules, not OntoUml, as TermIt does not support adding OntoUml rules
-                validator.getModelRules().stream().filter(r ->
-                        r.toString().contains("m1.ttl") || r.toString().contains("m2.ttl"))
-                        .collect(Collectors.toList())
-        );
-
         try {
-            final Model model = getModelFromRdf4jRepository(vocabularyIris);
-            org.topbraid.shacl.validation.ValidationReport report = validator.validate(model, rules);
+            final Model validationModel = initValidationModel(validator);
+            final Model dataModel = getModelFromRdf4jRepository(vocabularyIris);
+            org.topbraid.shacl.validation.ValidationReport report = validator.validate(dataModel, validationModel);
             LOG.debug("Done.");
             return report.results().stream()
-                    .sorted(new ValidationResultSeverityComparator()).map(result -> {
+                         .sorted(new ValidationResultSeverityComparator()).map(result -> {
                         final URI termUri = URI.create(result.getFocusNode().toString());
                         final URI severity = URI.create(result.getSeverity().getURI());
                         final URI errorUri = result.getSourceShape().isURIResource() ?
-                                URI.create(result.getSourceShape().getURI()) : null;
+                                             URI.create(result.getSourceShape().getURI()) : null;
                         final URI resultPath = result.getPath() != null && result.getPath().isURIResource() ?
-                                URI.create(result.getPath().getURI()) : null;
+                                               URI.create(result.getPath().getURI()) : null;
                         final MultilingualString messages = new MultilingualString(result.getMessages().stream()
-                                .map(RDFNode::asLiteral)
-                                .collect(Collectors.toMap(Literal::getLanguage, Literal::getLexicalForm)));
+                                                                                         .map(RDFNode::asLiteral)
+                                                                                         .collect(Collectors.toMap(
+                                                                                                 Literal::getLanguage,
+                                                                                                 Literal::getLexicalForm)));
 
                         return new ValidationResult()
                                 .setTermUri(termUri)
@@ -101,6 +117,36 @@ public class Validator implements VocabularyContentValidator {
                     }).collect(Collectors.toList());
         } catch (IOException e) {
             throw new TermItException("Validation of vocabularies " + vocabularyIris + " failed.", e);
+        }
+    }
+
+    private Model initValidationModel(com.github.sgov.server.Validator validator) throws IOException {
+        // TODO We could cache the validation model between calls
+        final Set<URL> rules = new HashSet<>();
+        rules.addAll(validator.getGlossaryRules().stream()
+                              .filter(r -> GLOSSARY_RULES_TO_OVERRIDE.stream().noneMatch(s -> r.toString().contains(s)))
+                              .collect(Collectors.toSet()));
+        rules.addAll(
+                // Currently, only using content rules, not OntoUml, as TermIt does not support adding OntoUml rules
+                validator.getModelRules().stream()
+                         .filter(r -> MODEL_RULES_TO_ADD.stream().anyMatch(s -> r.toString().contains(s)))
+                         .collect(Collectors.toList())
+        );
+        final Model validationModel = com.github.sgov.server.Validator.getRulesModel(rules);
+        loadOverrideRules(validationModel);
+        return validationModel;
+    }
+
+    private void loadOverrideRules(Model validationModel) throws IOException {
+        final ClassLoader classLoader = Validator.class.getClassLoader();
+        final PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(classLoader);
+
+        Resource[] resources = resolver.getResources("classpath:/validation/*.ttl");
+        for (Resource r : resources) {
+            String rule = Utils.loadClasspathResource("validation/" + r.getFilename());
+            rule = rule.replace("$lang", language);
+            validationModel.read(new ByteArrayInputStream(rule.getBytes(StandardCharsets.UTF_8)), null,
+                                 FileUtils.langTurtle);
         }
     }
 }
---
+++ src/test/java/cz/cvut/kbss/termit/persistence/validation/ValidatorTest.java
@@ -0,0 +1,71 @@
+package cz.cvut.kbss.termit.persistence.validation;
+
+import cz.cvut.kbss.jopa.model.EntityManager;
+import cz.cvut.kbss.termit.environment.Environment;
+import cz.cvut.kbss.termit.environment.Generator;
+import cz.cvut.kbss.termit.model.Term;
+import cz.cvut.kbss.termit.model.User;
+import cz.cvut.kbss.termit.model.UserAccount;
+import cz.cvut.kbss.termit.model.Vocabulary;
+import cz.cvut.kbss.termit.model.validation.ValidationResult;
+import cz.cvut.kbss.termit.persistence.context.DescriptorFactory;
+import cz.cvut.kbss.termit.persistence.dao.BaseDaoTestRunner;
+import cz.cvut.kbss.termit.util.Configuration;
+import cz.cvut.kbss.termit.util.Constants;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorTest extends BaseDaoTestRunner {
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private DescriptorFactory descriptorFactory;
+
+    @Autowired
+    private Configuration config;
+
+    @BeforeEach
+    void setUp() {
+        final User author = Generator.generateUserWithId();
+        transactional(() -> em.persist(author));
+        Environment.setCurrentUser(author);
+    }
+
+    @Test
+    void validateUsesOverrideRulesToAllowI18n() {
+        final Vocabulary vocabulary = generateVocabulary();
+        transactional(() -> {
+            final Validator sut = new Validator(em, config);
+            final List<ValidationResult> result = sut.validate(Collections.singleton(vocabulary.getUri()));
+            assertTrue(result.stream().noneMatch(
+                    vr -> vr.getMessage().get("en").contains("The term does not have a preferred label in Czech")));
+            assertTrue(result.stream().noneMatch(
+                    vr -> vr.getMessage().get("en").contains("The term does not have a definition in Czech")));
+            assertTrue(result.stream().anyMatch(vr -> vr.getMessage().get("en").contains(
+                    "The term does not have a preferred label in the primary configured language of this deployment of TermIt")));
+        });
+    }
+
+    private Vocabulary generateVocabulary() {
+        final Vocabulary vocabulary = Generator.generateVocabularyWithId();
+        final Term term = Generator.generateTermWithId(vocabulary.getUri());
+        term.getLabel().remove(Constants.DEFAULT_LANGUAGE);
+        term.getLabel().set("de", "Apfelbaum, der");
+        vocabulary.getGlossary().addRootTerm(term);
+        transactional(() -> {
+            em.persist(vocabulary, descriptorFactory.vocabularyDescriptor(vocabulary));
+            em.persist(term, descriptorFactory.termDescriptor(vocabulary));
+        });
+        return vocabulary;
+    }
+}
