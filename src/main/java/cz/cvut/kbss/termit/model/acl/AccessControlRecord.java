27ebbeafcb8d4fd0d53e754a3e82aa8c15ecedcd
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2023-03-07 08:55:35 +0100

    [kbss-cvut/termit-ui#368] Update ACL model to use owl:oneOf for mapping AccessLevel values.

--- src/main/java/cz/cvut/kbss/termit/model/acl/AccessControlRecord.java
+++ src/main/java/cz/cvut/kbss/termit/model/acl/AccessControlRecord.java
@@ -1,15 +1,14 @@
 package cz.cvut.kbss.termit.model.acl;
 
-import cz.cvut.kbss.jopa.model.annotations.OWLClass;
-import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
+import cz.cvut.kbss.jopa.model.annotations.*;
 import cz.cvut.kbss.termit.model.AbstractEntity;
 import cz.cvut.kbss.termit.util.Vocabulary;
 
 @OWLClass(iri = Vocabulary.s_c_zaznam_rizeni_pristupu)
 public abstract class AccessControlRecord extends AbstractEntity {
 
-    // TODO Change to OWLObjectProperty when owl:oneOf mapping of enums is supported by JOPA
-    @OWLDataProperty(iri = Vocabulary.s_p_ma_uroven_pristupovych_opravneni)
+    @Enumerated(EnumType.OBJECT_ONE_OF)
+    @OWLObjectProperty(iri = Vocabulary.s_p_ma_uroven_pristupovych_opravneni)
     private AccessLevel accessLevel;
 
     public AccessLevel getAccessLevel() {
--- src/main/java/cz/cvut/kbss/termit/model/acl/AccessLevel.java
+++ src/main/java/cz/cvut/kbss/termit/model/acl/AccessLevel.java
@@ -1,30 +1,35 @@
 package cz.cvut.kbss.termit.model.acl;
 
+import cz.cvut.kbss.jopa.model.annotations.Individual;
+import cz.cvut.kbss.termit.util.Vocabulary;
+
 import java.util.Set;
 
 /**
  * Levels of access to an asset.
  * <p>
  * The access levels are hierarchical, i.e., higher levels of access include lower (more restricted) levels.
- *
- * TODO Map to individuals from owl:oneOf
  */
 public enum AccessLevel {
     /**
      * The most restricted access level. The asset is not even visible to the user.
      */
+    @Individual(iri = Vocabulary.s_i_zadna)
     NONE(),
     /**
      * Read access to an asset. May include exporting, commenting, or snapshot display.
      */
+    @Individual(iri = Vocabulary.s_i_cteni)
     READ(NONE),
     /**
      * Write access to an asset. The user can edit the asset.
      */
+    @Individual(iri = Vocabulary.s_i_zapis)
     WRITE(NONE, READ),
     /**
      * User can edit or remove an asset and manage access of other users/user groups to it.
      */
+    @Individual(iri = Vocabulary.s_i_sprava)
     SECURITY(NONE, READ, WRITE);
 
     private final Set<AccessLevel> included;
