1801e4846e181ba44a42dc0dcdcc808f5ae1fea2
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2023-05-15 12:53:30 +0200

    [Documentation #228] Add Open API documentation to more REST controllers.

--- src/main/java/cz/cvut/kbss/termit/rest/TermOccurrenceController.java
+++ src/main/java/cz/cvut/kbss/termit/rest/TermOccurrenceController.java
@@ -5,6 +5,12 @@ import cz.cvut.kbss.termit.service.IdentifierResolver;
 import cz.cvut.kbss.termit.service.business.TermOccurrenceService;
 import cz.cvut.kbss.termit.util.Configuration;
 import cz.cvut.kbss.termit.util.Constants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
+@Tag(name = "Term occurrences", description = "Term occurrence management API")
 @RestController
 @RequestMapping(TermOccurrenceController.PATH)
 public class TermOccurrenceController extends BaseController {
@@ -24,7 +31,6 @@ public class TermOccurrenceController extends BaseController {
      */
     public static final String PATH = "/occurrence";
 
-
     private final TermOccurrenceService occurrenceService;
 
     public TermOccurrenceController(IdentifierResolver idResolver, Configuration config,
@@ -33,24 +39,55 @@ public class TermOccurrenceController extends BaseController {
         this.occurrenceService = occurrenceService;
     }
 
-    @PutMapping(value = "/{normalizedName}")
+    @Operation(security = {@SecurityRequirement(name = "bearer-key")},
+               description = "Approves a suggested term occurrence with the specified identifier.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Term occurrence approved."),
+            @ApiResponse(responseCode = "404", description = "Term occurrence not found.")
+    })
+    @PutMapping(value = "/{localName}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("hasRole('" + SecurityConstants.ROLE_FULL_USER + "')")
-    public void approveOccurrence(@PathVariable String normalizedName,
-                                  @RequestParam(name = Constants.QueryParams.NAMESPACE) String namespace) {
-        final URI identifier = idResolver.resolveIdentifier(namespace, normalizedName);
+    public void approveOccurrence(
+            @Parameter(description = TermOccurrenceControllerDoc.ID_LOCAL_NAME_DESCRIPTION,
+                       example = TermOccurrenceControllerDoc.ID_LOCAL_NAME_EXAMPLE)
+            @PathVariable String localName,
+            @Parameter(description = TermOccurrenceControllerDoc.ID_NAMESPACE_DESCRIPTION,
+                       example = TermOccurrenceControllerDoc.ID_NAMESPACE_EXAMPLE)
+            @RequestParam(name = Constants.QueryParams.NAMESPACE) String namespace) {
+        final URI identifier = idResolver.resolveIdentifier(namespace, localName);
 
         occurrenceService.approve(occurrenceService.getRequiredReference(identifier));
         LOG.debug("Occurrence with identifier <{}> approved.", identifier);
     }
 
-    @DeleteMapping(value = "/{normalizedName}")
+    @Operation(security = {@SecurityRequirement(name = "bearer-key")},
+               description = "Rejects a suggested term occurrence with the specified identifier.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Term occurrence rejected."),
+            @ApiResponse(responseCode = "404", description = "Term occurrence not found.")
+    })
+    @DeleteMapping(value = "/{localName}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("hasRole('" + SecurityConstants.ROLE_FULL_USER + "')")
-    public void removeOccurrence(@PathVariable String normalizedName,
+    public void removeOccurrence(@Parameter(description = TermOccurrenceControllerDoc.ID_LOCAL_NAME_DESCRIPTION,
+                                            example = TermOccurrenceControllerDoc.ID_LOCAL_NAME_EXAMPLE)
+                                 @PathVariable String localName,
+                                 @Parameter(description = TermOccurrenceControllerDoc.ID_NAMESPACE_DESCRIPTION,
+                                            example = TermOccurrenceControllerDoc.ID_NAMESPACE_EXAMPLE)
                                  @RequestParam(name = Constants.QueryParams.NAMESPACE) String namespace) {
-        final URI identifier = idResolver.resolveIdentifier(namespace, normalizedName);
+        final URI identifier = idResolver.resolveIdentifier(namespace, localName);
         occurrenceService.remove(occurrenceService.getRequiredReference(identifier));
         LOG.debug("Occurrence with identifier <{}> removed.", identifier);
+    }
+
+    /**
+     * A couple of constants for the {@link TermOccurrenceController} API documentation.
+     */
+    private static final class TermOccurrenceControllerDoc {
+        private static final String ID_LOCAL_NAME_DESCRIPTION = "Locally (in the context of the specified namespace) unique part of the term occurrence identifier.";
+        private static final String ID_LOCAL_NAME_EXAMPLE = "instance-12345";
+        private static final String ID_NAMESPACE_DESCRIPTION = "Term occurrence identifier namespace.";
+        private static final String ID_NAMESPACE_EXAMPLE = "http://onto.fel.cvut.cz/ontologies/application/termit/pojem/v\u00fdskyt-termu/";
     }
 }
