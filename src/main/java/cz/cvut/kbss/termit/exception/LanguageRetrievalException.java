bb1f3e4bb2214a82e45c89253a631be05ed61fd8
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2023-10-31 17:47:58 +0100

    [Chore] Generate license headers using regular Java comments.
    This should prevent unwanted format changes in headers when code is reformatted.

--- src/main/java/cz/cvut/kbss/termit/exception/LanguageRetrievalException.java
+++ src/main/java/cz/cvut/kbss/termit/exception/LanguageRetrievalException.java
@@ -1,6 +1,6 @@
-/**
+/*
  * TermIt
- * Copyright (C) 2020 Czech Technical University in Prague
+ * Copyright (C) 2023 Czech Technical University in Prague
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
