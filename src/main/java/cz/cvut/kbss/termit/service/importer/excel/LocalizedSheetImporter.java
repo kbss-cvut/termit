733bbe48ec0e94edd5b2a2afff9871abaf3625e8
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-06-25 16:13:37 +0200

    [kbss-cvut/termit-ui#449] Support importing multilingual Excel without term references.

--- src/main/java/cz/cvut/kbss/termit/service/importer/excel/LocalizedSheetImporter.java
+++ src/main/java/cz/cvut/kbss/termit/service/importer/excel/LocalizedSheetImporter.java
@@ -4,7 +4,6 @@ import com.neovisionaries.i18n.LanguageCode;
 import cz.cvut.kbss.jopa.model.MultilingualString;
 import cz.cvut.kbss.jopa.vocabulary.DC;
 import cz.cvut.kbss.jopa.vocabulary.SKOS;
-import cz.cvut.kbss.termit.exception.importing.VocabularyImportException;
 import cz.cvut.kbss.termit.model.Term;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -16,6 +15,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -23,6 +23,8 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,12 +32,22 @@ class LocalizedSheetImporter {
 
     private static final Logger LOG = LoggerFactory.getLogger(LocalizedSheetImporter.class);
 
+    private final List<Term> existingTerms;
+
     private Map<String, Integer> attributeToColumn;
+    private String langTag;
+
+    LocalizedSheetImporter(List<Term> existingTerms) {
+        this.existingTerms = existingTerms;
+    }
 
     List<Term> resolveTermsFromSheet(Sheet sheet) {
         LOG.debug("Importing terms from sheet '{}'.", sheet.getSheetName());
-        final LanguageCode lang = resolveLanguage(sheet);
-        final String langTag = lang.name();
+        final Optional<LanguageCode> lang = resolveLanguage(sheet);
+        if (lang.isEmpty()) {
+            return existingTerms;
+        }
+        this.langTag = lang.get().name();
         LOG.trace("Sheet '{}' mapped to language tage '{}'.", sheet.getSheetName(), langTag);
         final Properties attributeMapping = new Properties();
         final Map<String, Term> labelToTerm = new LinkedHashMap<>();
@@ -50,41 +62,66 @@ class LocalizedSheetImporter {
         }
         for (int i = 1; i < sheet.getLastRowNum(); i++) {
             final Row termRow = sheet.getRow(i);
-            final Term term = new Term();
+            Term term = existingTerms.size() >= i ? existingTerms.get(i - 1) : new Term();
             final Optional<String> label = getAttributeValue(termRow, SKOS.PREF_LABEL);
             if (label.isEmpty()) {
                 LOG.trace("Reached empty label column cell at row {}. Finished processing sheet.", i);
                 break;
             }
-            term.setLabel(MultilingualString.create(label.get(), langTag));
-            getAttributeValue(termRow, SKOS.DEFINITION).ifPresent(
-                    d -> term.setDefinition(MultilingualString.create(d, langTag)));
-            getAttributeValue(termRow, SKOS.SCOPE_NOTE).ifPresent(
-                    sn -> term.setDescription(MultilingualString.create(sn, langTag)));
-            getAttributeValue(termRow, SKOS.ALT_LABEL).ifPresent(al -> term.setAltLabels(
-                    splitIntoMultipleValues(al).stream().map(s -> MultilingualString.create(s, langTag)).collect(
-                            Collectors.toSet())));
-            getAttributeValue(termRow, SKOS.HIDDEN_LABEL).ifPresent(hl -> term.setHiddenLabels(
-                    splitIntoMultipleValues(hl).stream().map(s -> MultilingualString.create(s, langTag)).collect(
-                            Collectors.toSet())));
-            getAttributeValue(termRow, SKOS.EXAMPLE).ifPresent(ex -> term.setExamples(
-                    splitIntoMultipleValues(ex).stream().map(s -> MultilingualString.create(s, langTag)).collect(
-                            Collectors.toSet())));
-            getAttributeValue(termRow, DC.Terms.SOURCE).ifPresent(src -> term.setSources(splitIntoMultipleValues(src)));
-            getAttributeValue(termRow, SKOS.NOTATION).ifPresent(nt -> term.setNotations(splitIntoMultipleValues(nt)));
-            getAttributeValue(termRow, DC.Terms.REFERENCES).ifPresent(
-                    nt -> term.setProperties(Map.of(DC.Terms.REFERENCES, splitIntoMultipleValues(nt))));
+            mapRowToTerm(term, label.get(), termRow);
             labelToTerm.put(label.get(), term);
         }
         return new ArrayList<>(labelToTerm.values());
     }
 
-    private static LanguageCode resolveLanguage(Sheet sheet) {
+    private void mapRowToTerm(Term term, String label, Row termRow) {
+        initSingularMultilingualString(term::getLabel, term::setLabel).set(langTag, label);
+        getAttributeValue(termRow, SKOS.DEFINITION).ifPresent(
+                d -> initSingularMultilingualString(term::getDefinition, term::setDefinition).set(langTag, d));
+        getAttributeValue(termRow, SKOS.SCOPE_NOTE).ifPresent(
+                sn -> initSingularMultilingualString(term::getDescription, term::setDescription).set(langTag, sn));
+        getAttributeValue(termRow, SKOS.ALT_LABEL).ifPresent(al -> populatePluralMultilingualString(term::getAltLabels, term::setAltLabels, splitIntoMultipleValues(al)));
+        getAttributeValue(termRow, SKOS.HIDDEN_LABEL).ifPresent(hl -> populatePluralMultilingualString(term::getHiddenLabels, term::setHiddenLabels, splitIntoMultipleValues(hl)));
+        getAttributeValue(termRow, SKOS.EXAMPLE).ifPresent(ex -> populatePluralMultilingualString(term::getExamples, term::setExamples, splitIntoMultipleValues(ex)));
+        getAttributeValue(termRow, DC.Terms.SOURCE).ifPresent(src -> term.setSources(splitIntoMultipleValues(src)));
+        getAttributeValue(termRow, SKOS.NOTATION).ifPresent(nt -> term.setNotations(splitIntoMultipleValues(nt)));
+        getAttributeValue(termRow, DC.Terms.REFERENCES).ifPresent(
+                nt -> term.setProperties(Map.of(DC.Terms.REFERENCES, splitIntoMultipleValues(nt))));
+    }
+
+    private MultilingualString initSingularMultilingualString(Supplier<MultilingualString> getter,
+                                                              Consumer<MultilingualString> setter) {
+        if (getter.get() == null) {
+            setter.accept(new MultilingualString());
+        }
+        return getter.get();
+    }
+
+    private void populatePluralMultilingualString(
+            Supplier<Set<MultilingualString>> getter, Consumer<Set<MultilingualString>> setter, Set<String> values) {
+        Set<MultilingualString> attValue = getter.get();
+        if (attValue == null) {
+            setter.accept(new HashSet<>());
+            attValue = getter.get();
+        }
+        for (String s : values) {
+            final Optional<MultilingualString> mls = attValue.stream().filter(m -> !m.contains(langTag)).findFirst();
+            if (mls.isPresent()) {
+                mls.get().set(langTag, s);
+            } else {
+                final MultilingualString newMls = MultilingualString.create(s, langTag);
+                attValue.add(newMls);
+            }
+        }
+    }
+
+    private static Optional<LanguageCode> resolveLanguage(Sheet sheet) {
         final List<LanguageCode> codes = LanguageCode.findByName(sheet.getSheetName());
         if (codes.isEmpty()) {
-            throw new VocabularyImportException("Unsupported sheet language " + sheet.getSheetName());
+            LOG.debug("No matching language found for sheet '{}'. Skipping it.", sheet.getSheetName());
+            return Optional.empty();
         }
-        return codes.get(0);
+        return Optional.of(codes.get(0));
     }
 
     private Map<String, Integer> resolveAttributeColumns(Row attributes, Properties attributeMapping) {
