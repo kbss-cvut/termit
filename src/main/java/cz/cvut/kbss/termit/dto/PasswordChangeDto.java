efeb67063ec96f835bba2ad074e60d4aeaaffc21
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-07-18 17:03:01 +0200

    Provide JSON-LD mapping for PasswordChangeDto.

--- src/main/java/cz/cvut/kbss/termit/dto/PasswordChangeDto.java
+++ src/main/java/cz/cvut/kbss/termit/dto/PasswordChangeDto.java
@@ -1,13 +1,25 @@
 package cz.cvut.kbss.termit.dto;
 
+import cz.cvut.kbss.jopa.model.annotations.Id;
+import cz.cvut.kbss.jopa.model.annotations.OWLClass;
+import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
+import cz.cvut.kbss.jopa.vocabulary.DC;
+import cz.cvut.kbss.termit.util.Vocabulary;
+
 import java.io.Serializable;
 import java.net.URI;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 
+@OWLClass(iri = Vocabulary.ONTOLOGY_IRI_TERMIT + "/password-change")
 public class PasswordChangeDto implements Serializable {
+    @Id
     private URI uri;
+
+    @OWLDataProperty(iri = DC.Terms.IDENTIFIER)
     private String token;
+
+    @OWLDataProperty(iri = Vocabulary.s_p_ma_heslo)
     private String newPassword;
 
     public URI getUri() {
