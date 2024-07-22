1b6f9fb44a445e9c22108ec2de2992af8914289c
lukaskabc <lukaskabc@gmail.com>
2024-07-22 17:26:43 +0200

    [Enhancement #482] backend endpoint

--- src/main/java/cz/cvut/kbss/termit/service/notification/PasswordChangeNotifier.java
+++ src/main/java/cz/cvut/kbss/termit/service/notification/PasswordChangeNotifier.java
@@ -16,6 +16,7 @@ import java.util.Map;
 @Component
 public class PasswordChangeNotifier {
     private static final String PASSWORD_RESET_TEMPLATE = "password-change.vm";
+    private static final String CREATE_PASSWORD_TEMPLATE = "password-create.vm";
 
     private final Configuration config;
     private final MessageComposer messageComposer;
@@ -27,7 +28,7 @@ public class PasswordChangeNotifier {
         this.postman = postman;
     }
 
-    private String buildResetLink(PasswordChangeRequest request) {
+    private String buildPasswordResetLink(PasswordChangeRequest request) {
         return UriComponentsBuilder.fromHttpUrl(config.getUrl())
                                    .fragment("/reset-password/" +
                                                      request.getToken() + "/" +
@@ -36,29 +37,43 @@ public class PasswordChangeNotifier {
                                    ).toUriString();
     }
 
+    private String buildCreatePasswordLink(PasswordChangeRequest request) {
+        return UriComponentsBuilder.fromHttpUrl(config.getUrl())
+                                   .fragment("/create-password/" +
+                                           request.getToken() + "/" +
+                                           URLEncoder.encode(request.getUri().toString(),
+                                                   StandardCharsets.UTF_8)
+                                   ).toUriString();
+    }
+
     /**
-     * Creates message from ${@link #PASSWORD_RESET_TEMPLATE} template.
+     * Creates a message from specified template and frontend link.
      *
      * @param request Password reset request
+     * @param frontendLink a link to the frontend, mapped as {@code resetLink} for the template
+     * @param templateName Name of the template
      * @return message
      */
-    private Message createMessage(PasswordChangeRequest request) {
+    private Message createMessage(PasswordChangeRequest request, String frontendLink, String templateName) {
         Map<String, Object> variables = Map.of(
-                "resetLink", buildResetLink(request),
+                "resetLink", frontendLink,
                 "username", request.getUserAccount().getUsername(),
                 "validity", config.getSecurity().getPasswordChangeRequestValidity()
         );
         return Message.to(request.getUserAccount().getUsername())
-                      .content(messageComposer.composeMessage(PASSWORD_RESET_TEMPLATE, variables))
+                      .content(messageComposer.composeMessage(templateName, variables))
                       .subject(new MessageFormatter(config.getPersistence().getLanguage()).formatMessage(
                               "password-change.email.subject"))
                       .build();
     }
 
-    public void sendEmail(PasswordChangeRequest request) {
-        Message message = createMessage(request);
+    public void sendPasswordResetEmail(PasswordChangeRequest request) {
+        Message message = createMessage(request, buildPasswordResetLink(request), PASSWORD_RESET_TEMPLATE);
         postman.sendMessage(message);
     }
 
-
+    public void sendCreatePasswordEmail(PasswordChangeRequest request) {
+        Message message = createMessage(request, buildCreatePasswordLink(request), CREATE_PASSWORD_TEMPLATE);
+        postman.sendMessage(message);
+    }
 }
