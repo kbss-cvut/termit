d2e4822e89b0b0e3144fa0f26b7585f4533c9ef5
lukaskabc <lukaskabc@gmail.com>
2024-09-03 14:01:27 +0200

    add implSpec for exception handlers

--- src/main/java/cz/cvut/kbss/termit/rest/handler/RestExceptionHandler.java
+++ src/main/java/cz/cvut/kbss/termit/rest/handler/RestExceptionHandler.java
@@ -56,6 +56,7 @@ import jakarta.servlet.http.HttpServletRequest;
  * The general pattern should be that unless an exception can be handled in a more appropriate place it bubbles up to a
  * REST controller which originally received the request. There, it is caught by this handler, logged and a reasonable
  * error message is returned to the user.
+ * @implSpec Should reflect {@link cz.cvut.kbss.termit.websocket.handler.WebSocketExceptionHandler}
  */
 @RestControllerAdvice
 public class RestExceptionHandler {
