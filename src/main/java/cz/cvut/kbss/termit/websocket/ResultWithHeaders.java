fc978bcbe55f55094f5a43a818dc74c5ef8c8ea4
lukaskabc <lukaskabc@gmail.com>
2024-09-03 13:44:05 +0200

    update javadoc

--- src/main/java/cz/cvut/kbss/termit/websocket/ResultWithHeaders.java
+++ src/main/java/cz/cvut/kbss/termit/websocket/ResultWithHeaders.java
@@ -1,9 +1,11 @@
 package cz.cvut.kbss.termit.websocket;
 
 import cz.cvut.kbss.termit.websocket.handler.WebSocketMessageWithHeadersValueHandler;
+import org.springframework.messaging.handler.invocation.HandlerMethodReturnValueHandler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SendToUser;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -14,12 +16,19 @@ import java.util.Map;
  * including the {@link #payload}, {@link #destination} and {@link #headers} for the resulting message.
  * <p>
  * Do not combine with other method-return-value handlers (like {@link SendTo @SendTo})
+ * <p>
+ * The {@code ResultWithHeaders} is then handled by {@link WebSocketMessageWithHeadersValueHandler}.
+ * Every value returned from a controller method
+ * can be handled only by a single {@link HandlerMethodReturnValueHandler}.
+ * Annotations like {@link SendTo @SendTo}/{@link SendToUser @SendToUser}
+ * are handled by separate return value handlers, so only one can be used simultaneously.
  *
  * @param payload     The actual result of the method
  * @param destination The destination channel where the message will be sent
  * @param headers     Headers that will overwrite headers in the message.
  * @param <T>         The type of the payload
- * @see WebSocketMessageWithHeadersValueHandler processes results from methods
+ * @see WebSocketMessageWithHeadersValueHandler
+ * @see HandlerMethodReturnValueHandler
  */
 public record ResultWithHeaders<T>(T payload, @NotNull String destination, @NotNull Map<String, String> headers,
                                    boolean toUser) {
