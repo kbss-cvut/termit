bdd0c409b91c15d3780afa64bfc9202bd81dee84
lukaskabc <lukaskabc@gmail.com>
2024-09-09 07:20:22 +0200

    [Performance #287] test for calling web socket exception handler

--- src/test/java/cz/cvut/kbss/termit/websocket/WebSocketExceptionHandlerTest.java
+++ src/test/java/cz/cvut/kbss/termit/websocket/WebSocketExceptionHandlerTest.java
@@ -2,8 +2,10 @@ package cz.cvut.kbss.termit.websocket;
 
 import cz.cvut.kbss.termit.environment.Environment;
 import cz.cvut.kbss.termit.environment.Generator;
+import cz.cvut.kbss.termit.exception.PersistenceException;
 import cz.cvut.kbss.termit.websocket.handler.WebSocketExceptionHandler;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.messaging.simp.stomp.StompCommand;
@@ -13,10 +15,14 @@ import org.springframework.security.core.Authentication;
 
 import java.util.HashMap;
 
-class WebSocketExceptionHandlerTest extends BaseWebSocketControllerTestRunner {
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.notNull;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-    @SpyBean
-    WebSocketExceptionHandler sut;
+class WebSocketExceptionHandlerTest extends BaseWebSocketControllerTestRunner {
 
     @MockBean
     VocabularySocketController controller;
@@ -40,12 +46,12 @@ class WebSocketExceptionHandlerTest extends BaseWebSocketControllerTestRunner {
         this.serverInboundChannel.send(MessageBuilder.withPayload("").setHeaders(messageHeaders).build());
     }
 
-//    @Test // TODO
-//    void handlerIsCalledForPersistenceException() {
-//        final PersistenceException e = new PersistenceException(new Exception("mocked exception"));
-//        when(controller.validateVocabulary(any(), any())).thenThrow(e);
-//        sendMessage();
-//        verify(sut).persistenceException(notNull(), eq(e));
-//    }
+    @Test
+    void handlerIsCalledForPersistenceException() {
+        final PersistenceException e = new PersistenceException(new Exception("mocked exception"));
+        doThrow(e).when(controller).validateVocabulary(any(), any(), any());
+        sendMessage();
+        verify(webSocketExceptionHandler).persistenceException(notNull(), eq(e));
+    }
 
 }
