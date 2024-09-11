13fe4da75a3e883768c1c4f3a1caed825c5fa911
lukaskabc <lukaskabc@gmail.com>
2024-09-11 12:30:15 +0200

    [Performance #285] reset client when all long-running tasks completed

--- src/main/java/cz/cvut/kbss/termit/util/longrunning/LongRunningTasksRegistry.java
+++ src/main/java/cz/cvut/kbss/termit/util/longrunning/LongRunningTasksRegistry.java
@@ -1,10 +1,13 @@
 package cz.cvut.kbss.termit.util.longrunning;
 
+import cz.cvut.kbss.termit.event.AllLongRunningTasksCompletedEvent;
 import cz.cvut.kbss.termit.event.LongRunningTaskChangedEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.UnmodifiableView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +21,13 @@ public class LongRunningTasksRegistry {
     private static final Logger LOG = LoggerFactory.getLogger(LongRunningTasksRegistry.class);
 
     private final ConcurrentHashMap<String, LongRunningTaskStatus> registry = new ConcurrentHashMap<>();
+
+    private final ApplicationEventPublisher eventPublisher;
+
+    @Autowired
+    public LongRunningTasksRegistry(ApplicationEventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
+    }
 
     @EventListener
     public void onTaskChanged(LongRunningTaskChangedEvent event) {
@@ -33,6 +43,10 @@ public class LongRunningTasksRegistry {
             registry.remove(status.getName());
         } else {
             registry.put(status.getName(), status);
+        }
+        if (registry.isEmpty()) {
+            eventPublisher.publishEvent(new AllLongRunningTasksCompletedEvent(this));
+            LOG.trace("All long running tasks completed");
         }
     }
 
