42ad02d768c0a92f6e0d1f71f6d6ff5186b8c6f0
Lukáš Kaňka <lukaskabc@gmail.com>
2025-10-05 11:55:18 +0200

    [Enhancement #338] Finish refactoring existing test for BackupManager

---
+++ src/test/java/cz/cvut/kbss/termit/service/document/BaseDocumentTestRunner.java
@@ -0,0 +1,72 @@
+package cz.cvut.kbss.termit.service.document;
+
+import cz.cvut.kbss.termit.environment.Generator;
+import cz.cvut.kbss.termit.environment.PropertyMockingApplicationContextInitializer;
+import cz.cvut.kbss.termit.model.resource.Document;
+import cz.cvut.kbss.termit.model.resource.File;
+import cz.cvut.kbss.termit.service.BaseServiceTestRunner;
+import cz.cvut.kbss.termit.service.document.backup.BackupFileUtils;
+import cz.cvut.kbss.termit.service.document.backup.BackupReason;
+import cz.cvut.kbss.termit.util.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@ContextConfiguration(initializers = {PropertyMockingApplicationContextInitializer.class})
+public abstract class BaseDocumentTestRunner extends BaseServiceTestRunner {
+    protected static final String CONTENT =
+            "<html><body><h1>Metropolitan plan</h1><p>Description of the metropolitan plan.</body></html>";
+
+    @Autowired
+    protected Configuration configuration;
+    protected Document document;
+    protected Path documentDir;
+
+    @BeforeEach
+    protected void setUp() throws Exception {
+        this.document = new Document();
+        document.setLabel("Metropolitan plan");
+        document.setUri(Generator.generateUri());
+
+        Path termitStorageDir = Files.createTempDirectory("termit");
+        termitStorageDir.toFile().deleteOnExit();
+
+        documentDir = termitStorageDir.resolve(document.getDirectoryName());
+        documentDir.toFile().mkdir();
+        configuration.getFile().setStorage(termitStorageDir.toString());
+    }
+
+    protected java.io.File generateFile() throws Exception {
+        return generateFile("test", ".html", CONTENT);
+    }
+
+    protected java.io.File generateFile(String filePrefix, String fileSuffix, String fileContent) throws Exception {
+        final java.io.File content = Files.createTempFile(documentDir, filePrefix, fileSuffix).toFile();
+        content.deleteOnExit();
+        Files.write(content.toPath(), Collections.singletonList(fileContent));
+        return content;
+    }
+
+
+    protected List<java.io.File> createTestBackups(File file) throws Exception {
+        final List<java.io.File> backupFiles = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Instant instant = Instant.ofEpochMilli(System.currentTimeMillis() - (i + 1) * 10000);
+            final String newPath = BackupFileUtils.generateBackupFileName(file, BackupReason.UNKNOWN, instant);
+            final Path target = documentDir.resolve(newPath);
+            Files.createFile(target);
+            backupFiles.add(target.toFile());
+            target.toFile().deleteOnExit();
+        }
+        // So that the oldest is first
+        Collections.reverse(backupFiles);
+        return backupFiles;
+    }
+}
