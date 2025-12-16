80beb007689fbee17086a8e20ee79691dea745f9
Lukáš Kaňka <lukaskabc@gmail.com>
2025-12-16 16:45:17 +0100

    [Enhancement kbss-cvut/termit-ui#691] Accept personal access tokens for deletion using local name

--- src/main/java/cz/cvut/kbss/termit/rest/PersonalAccessTokenController.java
+++ src/main/java/cz/cvut/kbss/termit/rest/PersonalAccessTokenController.java
@@ -2,6 +2,7 @@ package cz.cvut.kbss.termit.rest;
 
 import cz.cvut.kbss.jsonld.JsonLd;
 import cz.cvut.kbss.termit.dto.PersonalAccessTokenDto;
+import cz.cvut.kbss.termit.model.PersonalAccessToken_;
 import cz.cvut.kbss.termit.security.SecurityConstants;
 import cz.cvut.kbss.termit.service.IdentifierResolver;
 import cz.cvut.kbss.termit.service.business.PersonalAccessTokenService;
@@ -61,11 +62,11 @@ public class PersonalAccessTokenController extends BaseController{
                description = "Deletes token with the specified identifier from the current user.")
     @ApiResponse(responseCode = "200", description = "Token deleted.")
     @ApiResponse(responseCode = "404", description = "Token not found.")
-    @DeleteMapping(PATH + "/{tokenUri}")
+    @DeleteMapping(PATH + "/{localName}")
     public void delete(
-            @Parameter(description = "Identifier of the token to delete.")
-            @PathVariable URI tokenUri) {
-        // TODO use identifier resolver?
+            @Parameter(description = "Local name of the token to delete.")
+            @PathVariable String localName) {
+        final URI tokenUri = resolveIdentifier(PersonalAccessToken_.entityClassIRI.toString(), localName);
         service.delete(tokenUri);
     }
 }
