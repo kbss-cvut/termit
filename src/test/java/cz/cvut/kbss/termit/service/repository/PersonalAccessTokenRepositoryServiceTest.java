66250297e01f4b8380a7540581901d2786a9ffb0
Lukáš Kaňka <lukaskabc@gmail.com>
2026-01-13 16:30:36 +0100

    [Fix] Do not initialize spring context for PersonalAccessTokenRepositoryServiceTest
    Hardcode the PAT entity URI in the repository service and manually initialize validator and DTO mapper in the test class.

--- src/test/java/cz/cvut/kbss/termit/service/repository/PersonalAccessTokenRepositoryServiceTest.java
+++ src/test/java/cz/cvut/kbss/termit/service/repository/PersonalAccessTokenRepositoryServiceTest.java
@@ -4,34 +4,30 @@ import cz.cvut.kbss.termit.dto.mapper.DtoMapper;
 import cz.cvut.kbss.termit.environment.Generator;
 import cz.cvut.kbss.termit.model.PersonalAccessToken;
 import cz.cvut.kbss.termit.persistence.dao.PersonalAccessTokenDao;
-import cz.cvut.kbss.termit.service.BaseServiceTestRunner;
-import jakarta.validation.Validator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.notNull;
 import static org.mockito.Mockito.doNothing;
 
 @ExtendWith(MockitoExtension.class)
-public class PersonalAccessTokenRepositoryServiceTest extends BaseServiceTestRunner {
+public class PersonalAccessTokenRepositoryServiceTest {
     @Mock
     private PersonalAccessTokenDao tokenDao;
-
-    @Autowired
-    private Validator validator;
-
-    @Autowired
-    private DtoMapper dtoMapper;
 
     private PersonalAccessTokenRepositoryService sut;
 
     @BeforeEach
     void setUp() {
+        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+        validator.afterPropertiesSet();
+        DtoMapper dtoMapper = Mappers.getMapper(DtoMapper.class);
         sut = new PersonalAccessTokenRepositoryService(validator, tokenDao, dtoMapper);
     }
 
