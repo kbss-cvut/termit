3ef598c9e229b61c3f439b2a0d257fb710bac2ea
Lukáš Kaňka <lukaskabc@gmail.com>
2026-08-19 08:32:55 +0200

    [Enhancement #418] Specify JsonLd attribute order for TermDescription and TermInfoWithParents

--- src/main/java/cz/cvut/kbss/termit/dto/TermDescription.java
+++ src/main/java/cz/cvut/kbss/termit/dto/TermDescription.java
@@ -1,6 +1,7 @@
 package cz.cvut.kbss.termit.dto;
 
 import cz.cvut.kbss.jopa.model.MultilingualString;
+import cz.cvut.kbss.jsonld.annotation.JsonLdAttributeOrder;
 import cz.cvut.kbss.termit.model.util.HasIdentifier;
 import cz.cvut.kbss.termit.model.util.HasTypes;
 
@@ -10,6 +11,7 @@ import java.net.URI;
 /**
  * Object holding a basic description of a Term
  */
+@JsonLdAttributeOrder({"uri", "label", "vocabulary", "state"})
 public interface TermDescription extends Serializable, HasIdentifier, HasTypes {
     MultilingualString getLabel();
     URI getVocabulary();
