862e6277b27d37bf8709e062377f80276c1e1fc4
Lukáš Kaňka <lukaskabc@gmail.com>
2026-08-28 09:00:09 +0200

    [Enhancement kbss-cvut/termit-ui#791] Consolidate term parents before exporting them into excel to include external parents in the export

--- src/main/java/cz/cvut/kbss/termit/service/export/ExcelVocabularyExporter.java
+++ src/main/java/cz/cvut/kbss/termit/service/export/ExcelVocabularyExporter.java
@@ -185,6 +185,7 @@ public class ExcelVocabularyExporter implements VocabularyExporter {
             final Row row = sheet.createRow(i + 1);
             row.setRowStyle(style);
             final Term t = terms.get(i);
+            t.consolidateParents(); // includes external parents in normal parents set
             resolvePrefixes(t, prefixes);
             termExporter.export(t, row);
             for (short j = 0; j < row.getLastCellNum(); j++) {
