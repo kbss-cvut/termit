50adae1b03b8133cf3ab5a1c4c91051637aab4f7
lukaskabc <lukaskabc@gmail.com>
2026-09-17 07:08:27 +0200

    [Ref] Update javadocs for term removal params and its applier
    Co-authored-by: Martin Ledvinka <martin.ledvinka@outlook.com>

--- src/main/java/cz/cvut/kbss/termit/service/repository/term_removal/TermRemovalParamsApplier.java
+++ src/main/java/cz/cvut/kbss/termit/service/repository/term_removal/TermRemovalParamsApplier.java
@@ -3,11 +3,11 @@ package cz.cvut.kbss.termit.service.repository.term_removal;
 import cz.cvut.kbss.termit.service.repository.TermRepositoryService;
 
 /**
- * Applier performing the given operation described by the parameters.
+ * Removes terms and executes any necessary related operations.
  */
 public interface TermRemovalParamsApplier {
     /**
-     * Applies the operation described by the parameters.
+     * Removes a term, performing additional operations based on the specified parameters.
      *
      * @param removalParams      Params describing how the term should be removed.
      * @param repositoryService  Service for interacting with the term repository
